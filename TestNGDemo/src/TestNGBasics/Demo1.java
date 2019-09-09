808b45619f30c092359a0b059e0ccedea97e955b
Admin <Admin@Admin-PC>
2019-09-09 23:17:40 +0530

    9/9/19

--- TestNGDemo/src/TestNGBasics/Demo1.java
+++ TestNGDemo/src/TestNGBasics/Demo1.java
@@ -1,5 +1,11 @@
 package TestNGBasics;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class Demo1 {
@@ -7,6 +13,41 @@ public class Demo1 {
 	void method1()
 	{
 		System.out.println("First MEthod");
+	}
+	
+	@BeforeSuite
+	void method2()
+	{
+		System.out.println("BeforeSuite");
+	}
+	@AfterSuite
+	void method3()
+	{
+		System.out.println("AfterSuite");
+	}
+	
+	@BeforeMethod
+	void method4()
+	{
+		System.out.println("BeforeMethod");
+	}
+	
+	@AfterMethod
+	void method5()
+	{
+		System.out.println("AfterMethod");
+	}
+	
+	@AfterClass
+	void method6()
+	{
+		System.out.println("AfterClass");
+	}
+	
+	@BeforeClass
+	void method7()
+	{
+		System.out.println("BeforeClass");
 	}
 
 }
---
+++ TestNGDemo/src/TestNGBasics/TestNGGroup.java
@@ -0,0 +1,43 @@
+package TestNGBasics;
+
+import org.testng.annotations.Test;
+
+public class TestNGGroup {
+
+	@Test(groups= {"default"})
+	void SanityGroup()
+	{
+		System.out.println("default group first method");
+	}
+	@Test(groups= {"Sanity","Reg"})
+	void SanityGroup1()
+	{
+		System.out.println("Sanity/Reg group first method");
+	}
+	
+	@Test(groups= {"Sanity"})
+	void regGroup()
+	{
+		System.out.println("Sanity group first method");
+	}
+	@Test(groups= {"Reg"})
+	void regGroup1()
+	{
+		System.out.println("Reg group first method");
+	}
+	
+	@Test(groups = {"endtoend"})
+    public void tc_Groups_05() {
+        System.out.println("tc_Groups_05 belonging to endtoend group.");
+    }
+    @Test(groups = {"endtoend"})
+    public void tc_Groups_06() {
+        System.out.println("tc_Groups_06 belonging to endtoend group.");
+    }
+    @Test(groups = {"volumetest"})
+    public void tc_Groups_07() {
+        System.out.println("tc_Groups_07 belonging to volumetest group.");
+    }
+	
+	
+}
